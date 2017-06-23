cddc90f83b059252804101bbc22d0b4b1bc83fd3
DimaGirya <DimaGirya1990@gmail.com>
2017-06-23 14:23:46 +0300

    code

---
+++ script.js
@@ -0,0 +1,160 @@
+const app = angular.module("graphicCourseHw3", []);
+app.controller("ctrl", function ($scope) {
+
+    let canvasContext;
+    let canvas;
+    let copyOfData;
+
+    window.onload = function () {
+        canvas = document.getElementById("workingZone");
+        canvasContext = canvas.getContext("2d");
+        copyOfData = angular.copy(data);
+        drawScene(data);
+    };
+
+    function drawPolygon(polygon, vertices) {
+        canvasContext.fillStyle = polygon.color;
+        canvasContext.beginPath();
+        let firstVertices = getVerticesById(vertices, polygon.vertices[0]);
+        let currentVertices = null;
+        let length = polygon.vertices.length;
+        canvasContext.moveTo(firstVertices.x, firstVertices.y);
+        for (let i = 1; i < length; i++) {
+            currentVertices = getVerticesById(vertices, polygon.vertices[i]);
+            canvasContext.lineTo(currentVertices.x, currentVertices.y);
+        }
+        canvasContext.lineTo(firstVertices.x, firstVertices.y);
+        canvasContext.fill();
+        canvasContext.closePath();
+    }
+
+    function rotatePolygonInX(polygon, angle, vertices) {
+        let length = polygon.vertices.length;
+        let currentVertices;
+        let y;
+        for (let i = 0; i < length; i++) {
+            currentVertices = getVerticesById(vertices, polygon.vertices[i]);
+            y = currentVertices.y;
+            currentVertices.y = currentVertices.y * Math.cos(angle) - currentVertices.z * Math.sin(angle);
+            currentVertices.z = y * Math.sin(angle) + currentVertices.z * Math.cos(angle);
+        }
+    }
+
+    function rotatePolygonInY(polygon, angle, vertices) {
+        let length = polygon.vertices.length;
+        let currentVertices;
+        let x;
+        for (let i = 0; i < length; i++) {
+            currentVertices = getVerticesById(vertices, polygon.vertices[i]);
+            x = currentVertices.x;
+            currentVertices.x = currentVertices.x * Math.cos(angle) + currentVertices.z * Math.sin(angle);
+            currentVertices.z = -x * Math.sin(angle) + currentVertices.z * Math.cos(angle);
+        }
+    }
+
+    function rotatePolygonInZ(polygon, angle, vertices) {
+
+    }
+
+    function getPolygonById(polygons, id) {
+        let result = null;
+        let length = polygons.length;
+        for (let i = 0; i < length; i++) {
+            if (polygons[i].id === id) {
+                result = polygons[i];
+                break;
+            }
+        }
+        if (result === null) {
+            throw "getPolygonById returned null"
+        }
+        return result;
+    }
+
+    function getVerticesById(vertices, id) {
+        let result = null;
+        let length = vertices.length;
+        for (let i = 0; i < length; i++) {
+            if (vertices[i].id === id) {
+                result = vertices[i];
+                break;
+            }
+        }
+        if (result === null) {
+            throw "getVerticesById returned null"
+        }
+        return result;
+    }
+
+    function drawShape(shape, polygons, vertices) {
+        let shapePolygonsIds = shape.polygons;
+        let polygon = null;
+        shapePolygonsIds.forEach(polygonId => {
+            polygon = getPolygonById(polygons, polygonId);
+            drawPolygon(polygon, vertices);
+        })
+    }
+
+    function rotateShapeX(shape, polygons, vertices, angle) {
+        let shapePolygonsIds = shape.polygons;
+        let polygon = null;
+        shapePolygonsIds.forEach(polygonId => {
+            polygon = getPolygonById(polygons, polygonId);
+            rotatePolygonInX(polygon, angle, vertices)
+        })
+    }
+
+    function rotateShapeY(shape, polygons, vertices, angle) {
+        let shapePolygonsIds = shape.polygons;
+        let polygon = null;
+        shapePolygonsIds.forEach(polygonId => {
+            polygon = getPolygonById(polygons, polygonId);
+            rotatePolygonInY(polygon, angle, vertices)
+        })
+    }
+
+    function rotateShapeZ(shape, polygons, vertices, angle) {
+        let shapePolygonsIds = shape.polygons;
+        let polygon = null;
+        shapePolygonsIds.forEach(polygonId => {
+            polygon = getPolygonById(polygons, polygonId);
+            rotatePolygonInZ(polygon, angle, vertices)
+        })
+    }
+
+
+    function clearBoard() {
+        canvasContext.clearRect(0, 0, 800, 800);
+    }
+
+
+    function drawScene(sceneData) {
+        let shapes = sceneData.shapes;
+        let vertices = sceneData.vertices;
+        let polygons = sceneData.polygons;
+        for (let i = 0; i < 100; i++) {
+            i++;
+            window.setTimeout(() => {
+                console.log(i);
+                clearBoard();
+                shapes.forEach(shapes => {
+                    rotateShapeY(shapes, polygons, vertices, i * 0.01);
+                    rotateShapeX(shapes, polygons, vertices, i * 0.01);
+                    drawShape(shapes, polygons, vertices);
+                });
+            }, 1000 * i);
+        }
+    }
+
+/*
+    function drawScene(sceneData) {
+        let shapes = sceneData.shapes;
+        let vertices = sceneData.vertices;
+        let polygons = sceneData.polygons;
+        shapes.forEach(shapes => {
+            rotateShapeY(shapes, polygons, vertices, 3);
+            drawShape(shapes, polygons, vertices);
+        });
+    }
+*/
+});
