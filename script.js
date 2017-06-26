493016ef87e55c1f2c22ae2169c7e41510d9b4ae
DimaGirya <DimaGirya1990@gmail.com>
2017-06-26 14:22:44 +0300

    refactor

--- script.js
+++ script.js
@@ -11,10 +11,20 @@ app.controller("ctrl", function ($scope) {
     $scope.scaleFactor = 0.5;
     $scope.rotateAngle = 45;
 
+    window.onload = function () {
+        canvas = document.getElementById("workingZone");
+        canvasContext = canvas.getContext("2d");
+        copyOfData = angular.copy(data);
+        normalizeData(data);
+        drawScene(data);
+    };
+
+    // -------------GUI functions--------------
+
     $scope.parallel = function () {
         normalizeData(data);
         data.shapes.forEach(shape => {
-            paralleProjectionShape(data,shape);
+            paralleProjectionShape(data, shape);
         });
         normalizeData(data);
         drawScene(data);
@@ -23,7 +33,7 @@ app.controller("ctrl", function ($scope) {
     $scope.cabinet = function () {
         normalizeData(data);
         data.shapes.forEach(shape => {
-            cabinetProjectionShape(data,shape);
+            cabinetProjectionShape(data, shape);
         });
         normalizeData(data);
         drawScene(data);
@@ -32,7 +42,7 @@ app.controller("ctrl", function ($scope) {
     $scope.prespctive = function () {
         normalizeData(data);
         data.shapes.forEach(shape => {
-            prespctiveProjectionShape(data,shape);
+            prespctiveProjectionShape(data, shape);
         });
         normalizeData(data);
         drawScene(data);
@@ -44,7 +54,6 @@ app.controller("ctrl", function ($scope) {
         normalizeData(data);
         drawScene(data);
     };
-
 
     $scope.rotateX = function () {
         normalizeData(data);
@@ -81,21 +90,182 @@ app.controller("ctrl", function ($scope) {
 
     $scope.zoom = function () {
         normalizeData(data);
-        let vertices = data.vertices;
-        zoomShapes(vertices, $scope.scaleFactor);
+        data.shapes.forEach(shape => {
+            zoomShapes(data, shape, $scope.scaleFactor);
+        });
         normalizeData(data);
         drawScene(data);
     };
 
-    window.onload = function () {
-        canvas = document.getElementById("workingZone");
-        canvasContext = canvas.getContext("2d");
-        copyOfData = angular.copy(data);
-        normalizeData(data);
-        drawScene(data);
-    };
+    // -------------------- Shape calculation level--------
 
+    function cabinetProjectionShape(data, shape) {
+        shape.polygons.forEach(polygonId => {
+            let polygon = getPolygonById(data.polygons, polygonId);
+            cabinetProjectionPolygon(data, polygon);
+        })
+    }
 
+    function paralleProjectionShape(data, shape) {
+        shape.polygons.forEach(polygonId => {
+            let polygon = getPolygonById(data.polygons, polygonId);
+            paralleProjectionPolygon(data, polygon);
+        })
+    }
+
+    function prespctiveProjectionShape(data, shape) {
+        shape.polygons.forEach(polygonId => {
+            let polygon = getPolygonById(data.polygons, polygonId);
+            prespctiveProjectionPolygon(data, polygon);
+        })
+    }
+
+    function zoomShapes(data, shape, scaleFactor) {
+        shape.polygons.forEach(polygonId => {
+            let polygon = getPolygonById(data.polygons, polygonId);
+            zoomPolygon(data, polygon, scaleFactor);
+        });
+
+    }
+
+    function rotateShapeX(shape, polygons, vertices, angle) {
+        let shapePolygonsIds = shape.polygons;
+        let polygon = null;
+        shapePolygonsIds.forEach(polygonId => {
+            polygon = getPolygonById(polygons, polygonId);
+            rotatePolygonInX(polygon, angle, vertices)
+        })
+    }
+
+    function rotateShapeY(shape, polygons, vertices, angle) {
+        let shapePolygonsIds = shape.polygons;
+        let polygon = null;
+        shapePolygonsIds.forEach(polygonId => {
+            polygon = getPolygonById(polygons, polygonId);
+            rotatePolygonInY(polygon, angle, vertices)
+        })
+    }
+
+    function rotateShapeZ(shape, polygons, vertices, angle) {
+        let shapePolygonsIds = shape.polygons;
+        let polygon = null;
+        shapePolygonsIds.forEach(polygonId => {
+            polygon = getPolygonById(polygons, polygonId);
+            rotatePolygonInZ(polygon, angle, vertices)
+        })
+    }
+
+    // -------------------- Polygons calculation level --------
+
+    function cabinetProjectionPolygon(data, polygon) {
+        polygon.vertices.forEach(vertexId => {
+            let vertex = getPolygonById(data.vertices, vertexId);
+            vertex.x = Math.round(vertex.x + vertex.z * Math.cos(Math.PI * (10 / 180)));
+            vertex.y = Math.round(vertex.y + vertex.z * Math.sin(Math.PI * (10 / 180)));
+        })
+    }
+
+    function paralleProjectionPolygon(data, polygon) {
+        let temp1;
+        let temp2;
+        polygon.vertices.forEach(vertexId => {
+            let vertex = getPolygonById(data.vertices, vertexId);
+            temp1 = vertex.x;
+            temp2 = vertex.y;
+            vertex.x = temp2;
+            vertex.y = temp1;
+        })
+    }
+
+    function prespctiveProjectionPolygon(data, polygon) {
+        polygon.vertices.forEach(vertexId => {
+            let vertex = getPolygonById(data.vertices, vertexId);
+            vertex.x = vertex.x + ( vertex.z / 10);
+            vertex.y = vertex.y + ( vertex.z / 10);
+        });
+    }
+
+    function zoomPolygon(data, polygon, scaleFactor) {
+        polygon.vertices.forEach(vertexId => {
+            let vertex = getPolygonById(data.vertices, vertexId);
+            vertex.x *= scaleFactor;
+            vertex.y *= scaleFactor;
+            vertex.z *= scaleFactor;
+        })
+    }
+
+    function rotatePolygonInX(polygon, angle, vertices) {
+        let length = polygon.vertices.length;
+        angle = angle * (Math.PI / 180);
+        let currentVertices;
+        let y;
+        for (let i = 0; i < length; i++) {
+            currentVertices = getVerticesById(vertices, polygon.vertices[i]);
+            y = currentVertices.y;
+            currentVertices.y = currentVertices.y * Math.cos(angle) - currentVertices.z * Math.sin(angle);
+            currentVertices.z = y * Math.sin(angle) + currentVertices.z * Math.cos(angle);
+        }
+    }
+
+    function rotatePolygonInY(polygon, angle, vertices) {
+        let length = polygon.vertices.length;
+        angle = angle * (Math.PI / 180);
+        let currentVertices;
+        let x;
+        for (let i = 0; i < length; i++) {
+            currentVertices = getVerticesById(vertices, polygon.vertices[i]);
+            x = currentVertices.x;
+            currentVertices.x = currentVertices.x * Math.cos(angle) + currentVertices.z * Math.sin(angle);
+            currentVertices.z = -x * Math.sin(angle) + currentVertices.z * Math.cos(angle);
+        }
+    }
+
+    function rotatePolygonInZ(polygon, angle, vertices) {
+        let length = polygon.vertices.length;
+        angle = angle * (Math.PI / 180);
+        let currentVertices;
+        let x;
+        for (let i = 0; i < length; i++) {
+            currentVertices = getVerticesById(vertices, polygon.vertices[i]);
+            x = currentVertices.x;
+            currentVertices.x = currentVertices.x * Math.cos(angle) - currentVertices.y * Math.sin(angle);
+            currentVertices.y = x * Math.sin(angle) + currentVertices.y * Math.cos(angle);
+        }
+    }
+
+    //------------ Get by id functions------------
+
+    function getPolygonById(polygons, id) {
+        let result = null;
+        let length = polygons.length;
+        for (let i = 0; i < length; i++) {
+            if (polygons[i].id === id) {
+                result = polygons[i];
+                break;
+            }
+        }
+        if (result === null) {
+            throw "getPolygonById returned null"
+        }
+        return result;
+    }
+
+    function getVerticesById(vertices, id) {
+        let result = null;
+        let length = vertices.length;
+        for (let i = 0; i < length; i++) {
+            if (vertices[i].id === id) {
+                result = vertices[i];
+                break;
+            }
+        }
+        if (result === null) {
+            throw "getVerticesById returned null"
+        }
+        return result;
+    }
+
+    // --------------- normalize data functions
     function resetCenterMinMax() {
         center = {
             x: 0,
@@ -162,158 +332,7 @@ app.controller("ctrl", function ($scope) {
         setMaxMinCenter(data);
     }
 
-    function rotatePolygonInX(polygon, angle, vertices) {
-        let length = polygon.vertices.length;
-        angle = angle * (Math.PI / 180);
-        let currentVertices;
-        let y;
-        for (let i = 0; i < length; i++) {
-            currentVertices = getVerticesById(vertices, polygon.vertices[i]);
-            y = currentVertices.y;
-            currentVertices.y = currentVertices.y * Math.cos(angle) - currentVertices.z * Math.sin(angle);
-            currentVertices.z = y * Math.sin(angle) + currentVertices.z * Math.cos(angle);
-        }
-    }
-
-    function rotatePolygonInY(polygon, angle, vertices) {
-        let length = polygon.vertices.length;
-        angle = angle * (Math.PI / 180);
-        let currentVertices;
-        let x;
-        for (let i = 0; i < length; i++) {
-            currentVertices = getVerticesById(vertices, polygon.vertices[i]);
-            x = currentVertices.x;
-            currentVertices.x = currentVertices.x * Math.cos(angle) + currentVertices.z * Math.sin(angle);
-            currentVertices.z = -x * Math.sin(angle) + currentVertices.z * Math.cos(angle);
-        }
-    }
-
-    function rotatePolygonInZ(polygon, angle, vertices) {
-        let length = polygon.vertices.length;
-        angle = angle * (Math.PI / 180);
-        let currentVertices;
-        let x;
-        for (let i = 0; i < length; i++) {
-            currentVertices = getVerticesById(vertices, polygon.vertices[i]);
-            x = currentVertices.x;
-            currentVertices.x = currentVertices.x * Math.cos(angle) - currentVertices.y * Math.sin(angle);
-            currentVertices.y = x * Math.sin(angle) + currentVertices.y * Math.cos(angle);
-        }
-    }
-
-    function getPolygonById(polygons, id) {
-        let result = null;
-        let length = polygons.length;
-        for (let i = 0; i < length; i++) {
-            if (polygons[i].id === id) {
-                result = polygons[i];
-                break;
-            }
-        }
-        if (result === null) {
-            throw "getPolygonById returned null"
-        }
-        return result;
-    }
-
-    function getVerticesById(vertices, id) {
-        let result = null;
-        let length = vertices.length;
-        for (let i = 0; i < length; i++) {
-            if (vertices[i].id === id) {
-                result = vertices[i];
-                break;
-            }
-        }
-        if (result === null) {
-            throw "getVerticesById returned null"
-        }
-        return result;
-    }
-
-    function rotateShapeX(shape, polygons, vertices, angle) {
-        let shapePolygonsIds = shape.polygons;
-        let polygon = null;
-        shapePolygonsIds.forEach(polygonId => {
-            polygon = getPolygonById(polygons, polygonId);
-            rotatePolygonInX(polygon, angle, vertices)
-        })
-    }
-
-    function rotateShapeY(shape, polygons, vertices, angle) {
-        let shapePolygonsIds = shape.polygons;
-        let polygon = null;
-        shapePolygonsIds.forEach(polygonId => {
-            polygon = getPolygonById(polygons, polygonId);
-            rotatePolygonInY(polygon, angle, vertices)
-        })
-    }
-
-    function rotateShapeZ(shape, polygons, vertices, angle) {
-        let shapePolygonsIds = shape.polygons;
-        let polygon = null;
-        shapePolygonsIds.forEach(polygonId => {
-            polygon = getPolygonById(polygons, polygonId);
-            rotatePolygonInZ(polygon, angle, vertices)
-        })
-    }
-
-    function zoomShapes(vertices, zoomFactor) {
-        vertices.forEach(vertex => {
-            vertex.x *= zoomFactor;
-            vertex.y *= zoomFactor;
-            vertex.z *= zoomFactor;
-        })
-    }
-
-    function cabinetProjectionShape(data,shape) {
-        shape.polygons.forEach(polygonId => {
-            let polygon = getPolygonById(data.polygons,polygonId);
-            cabinetProjectionPolygon(data,polygon);
-        })
-    }
-
-    function paralleProjectionShape(data,shape) {
-        shape.polygons.forEach(polygonId => {
-            let polygon = getPolygonById(data.polygons,polygonId);
-            paralleProjectionPolygon(data,polygon);
-        })
-    }
-
-    function prespctiveProjectionShape(data,shape){
-        shape.polygons.forEach(polygonId => {
-            let polygon = getPolygonById(data.polygons,polygonId);
-            prespctiveProjectionPolygon(data,polygon);
-        })
-    }
-
-    function cabinetProjectionPolygon(data,polygon) {
-        polygon.vertices.forEach(vertexId => {
-            let vertex = getPolygonById(data.vertices,vertexId);
-            vertex.x = Math.round(vertex.x + vertex.z * Math.cos(Math.PI * (10 / 180)));
-            vertex.y = Math.round(vertex.y + vertex.z * Math.sin(Math.PI * (10 / 180)));
-        })
-    }
-
-    function paralleProjectionPolygon(data,polygon) {
-        let temp1;
-        let temp2;
-        polygon.vertices.forEach(vertexId => {
-            let vertex = getPolygonById(data.vertices,vertexId);
-            temp1 = vertex.x;
-            temp2 = vertex.y;
-            vertex.x = temp2;
-            vertex.y = temp1;
-        })
-    }
-
-    function  prespctiveProjectionPolygon(data,polygon){
-        polygon.vertices.forEach(vertexId => {
-            let vertex = getPolygonById(data.vertices, vertexId);
-            vertex.x = vertex.x + ( vertex.z / 10);
-            vertex.y = vertex.y + ( vertex.z / 10);
-        });
-    }
+    // ------------- Drawing functions-------------
 
     function drawScene(sceneData) {
         let shapes = sceneData.shapes;
